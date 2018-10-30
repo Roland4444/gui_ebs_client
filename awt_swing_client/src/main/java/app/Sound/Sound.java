ea1b78041c6c5df879a64766922c167b2532c7c8
Roland4444 <turbopollux@rambler.ru>
2018-10-30 15:10:39 +0000

    enumerating all sources and targets impelmets; need select proper line to write it;

--- awt_swing_client/src/main/java/app/Sound/Sound.java
+++ awt_swing_client/src/main/java/app/Sound/Sound.java
@@ -1,20 +1,76 @@
 package app.Sound;
 
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.TargetDataLine;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Sound {
-    boolean checkAudioConfig(AudioFormat format) {
-
+    public Sound(Mixer mixer){
+        this.mixer=mixer;
+    }
+    public Mixer mixer = null;
+    public Thread ThreadRecord;
+    public String currentFile;
+    public TargetDataLine td;
+    public AudioFormat format;
+    public void setFormat(AudioFormat  f){
+        this.format=f;
+    }
+    boolean checkAudioConfig() {
         var info = new DataLine.Info(TargetDataLine.class, format);
         return AudioSystem.isLineSupported(info);
     }
-    boolean checkAudioConfig( AudioFormat.Encoding encoding, float sampleRate, int sampleSizeInBits,
-                                   int channels, int frameSize, float frameRate, boolean bigEndian){
-        var format = new AudioFormat(encoding, sampleRate, sampleSizeInBits, channels, frameSize, frameRate, bigEndian);
-        var info = new DataLine.Info(TargetDataLine.class, format);
-        return AudioSystem.isLineSupported(info);
+    public void startRecord(String FileName) throws LineUnavailableException, IOException {
+        var dataline = new DataLine.Info(TargetDataLine.class, format);
+        if (!checkAudioConfig()) {
+            System.out.println("FORMAt Not suppoerted");
+            return ;
+        }
+        DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+
+        td = (TargetDataLine) AudioSystem.getLine(info);;
+        td.open();
+        td.start();
+        ThreadRecord = new Thread(){
+            @Override
+            public void run(){
+                var audioStream = new AudioInputStream(td);
+                currentFile=FileName;
+                var file = new File(currentFile);
+                try {
+                    AudioSystem.write(audioStream, AudioFileFormat.Type.WAVE, file);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                };
+            }
+        };
+        ThreadRecord.start();
+    }
+    public void stopRecord(){
+        td.stop();
+        td.close();
+    }
+
+    public void getFullInfo(){
+        var mixInfos = AudioSystem.getMixerInfo();
+        var counter = 0;
+        for (var info : mixInfos){
+            System.out.println("\n\n\nIndex==>"+counter);
+            System.out.println("Name =>"+info.getName()+"\nDescription=>"+info.getDescription());
+            mixer = AudioSystem.getMixer(mixInfos[counter]);
+
+            System.out.println("Describing subsystem");
+            System.out.println("\nSOURCES::");
+            var sources = mixer.getSourceLineInfo();
+            for (var s : sources )
+                System.out.println(s.toString());
+
+            System.out.println("\nOUTPUTS::");
+            var targets = mixer.getTargetLineInfo();
+            for (var s : targets )
+                System.out.println(s.toString());
+
+            counter++;
+        }
     }
 }
--- awt_swing_client/src/test/java/app/Sound/SoundTest.java
+++ awt_swing_client/src/test/java/app/Sound/SoundTest.java
@@ -3,11 +3,18 @@ package app.Sound;
 import org.junit.jupiter.api.Test;
 
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Mixer;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SoundTest {
-
+    public Mixer.Info[] mixInfos = AudioSystem.getMixerInfo();
+    public Mixer mixer = AudioSystem.getMixer(mixInfos[6]);
+    Sound sound = new Sound(mixer);
     @Test
     void checkAudioConfig() {
     }
@@ -15,9 +22,20 @@ class SoundTest {
     @Test
     void checkAudioConfig1() {
         var newFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100, 24, 1, 4, 44100, true  );
-        var sound = new Sound();
-        assertEquals(true, sound.checkAudioConfig(newFormat));
+        sound.setFormat(newFormat);
+        assertEquals(true, sound.checkAudioConfig());
         var terribleFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 777000, 196, 2, 40, 5000000, true  );
-        assertEquals(false, sound.checkAudioConfig(terribleFormat));
+        sound.setFormat(terribleFormat);
+        assertEquals(false, sound.checkAudioConfig());
+    }
+
+    @Test
+    void startRecord() throws IOException, LineUnavailableException, InterruptedException {
+        var newFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100, 24, 1, 4, 44100, false  );
+        sound.setFormat(newFormat);
+        sound.startRecord("out.wav");
+        Thread.sleep(10000);
+        sound.stopRecord();
+
     }
 }
