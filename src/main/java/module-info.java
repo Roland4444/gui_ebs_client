cfcd09e23be9a6bd22c722802cb4eb5306f1270d
Roland4444 <turbopollux@rambler.ru>
2018-11-16 14:19:27 +0000

    clean up; retest video create from scratch

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,5 @@
 module se.roland.ebs_client{
-   requires opencv;
-   requires opencv.platform;
-   requires javacv;
-   requires javacv.platform;
+
    requires uk.roland.jaktor;
    requires uk.roland.ebs;
    requires java.desktop;
