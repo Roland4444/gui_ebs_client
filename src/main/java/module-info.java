e5aac8dfd787095fa1ea139f127ee263187c4092
Roland4444 <turbopollux@rambler.ru>
2018-11-16 10:55:33 +0000

    adding opencv

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,12 @@
 module se.roland.ebs_client{
-   requires webcam.capture.driver.openimaj;
+   requires opencv;
+   requires opencv.platform;
+   requires javacv;
+   requires javacv.platform;
    requires uk.roland.jaktor;
    requires uk.roland.ebs;
    requires java.desktop;
    requires java.uuid.generator;
-   requires webcam.capture;
-   requires webcam.capture.driver.ipcam;
+
    
 }
