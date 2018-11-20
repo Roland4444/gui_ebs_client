bfc2b919c2de693cfcdf3f493eaf2d799bf71df1
Roland4444 <turbopollux@rambler.ru>
2018-11-20 16:28:43 +0000

    j

--- src/test/java/app/GUIModules/Video/grabTest.java
+++ src/test/java/app/GUIModules/Video/grabTest.java
@@ -2,6 +2,11 @@ package app.GUIModules.Video;
 
 import org.junit.jupiter.api.Test;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,5 +18,23 @@ class grabTest {
         var grab = new grab();
         assertNotEquals(null, grab.vc);
         assertNotEquals(null, grab.getFrame());
+    }
+
+    @Test
+    void getImage() throws IOException, InterruptedException {
+        String image = "tested.png";
+        String wav = "temp.wav";
+        BufferedImage bf = ImageIO.read(new File(image));
+        assertNotEquals(null, bf);
+        Graphics g = bf.getGraphics();
+        JFrame jf = new JFrame("");
+        Canvas cns = new Canvas();
+        cns.paint(g);
+
+        jf.setSize(400, 300);
+        jf.getContentPane().add(cns);
+        jf.setVisible(true);
+        Thread.sleep(2000);
+
     }
 }
--- src/test/java/app/Sound/WavTest.java
+++ src/test/java/app/Sound/WavTest.java
@@ -3,7 +3,9 @@ package app.Sound;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,7 +27,33 @@ class WavTest {
     }
 
     @Test
-    void wavTest(){
+    void getwavSpectreTest() throws IOException {
+        var arr = Files.readAllBytes(new File("Silent.wav").toPath());
+        var rawdata  = Wav.getdata(arr);
+        PrintWriter pw = new PrintWriter("flow");
+        for (int i=0; i<rawdata.length;i++){
+            int uint8 = rawdata[i] & 0xFF;
+            pw.write(String.valueOf(uint8));
+            pw.write("\n");
+
+        }
+        pw.close();
+    }
+
+    @Test
+    void updateWAV() throws IOException {
+        var arr = Files.readAllBytes(new File("Silent.wav").toPath());
+        var header = Wav.getSubBytes(arr, 0, 43);
+        var data = new byte[arr.length-43];
+        for (int i=44; i<arr.length-1; i++){
+            data[i-44] = (byte) (i % 5000);
+            
+        };
+        var fos = new FileOutputStream("mod.wav");
+        fos.write(header);
+        fos.write(data);
+        fos.close();
+
 
     }
 }
