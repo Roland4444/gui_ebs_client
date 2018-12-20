6dff857beac25d73eb0def354760c127eabcd909
Roland4444 <turbopollux@rambler.ru>
2018-12-20 17:23:26 +0400

    upfrade

---
+++ src/main/java/app/GUIModules/Interface/UpgradeESIAFrame.java
@@ -0,0 +1,22 @@
+package app.GUIModules.Interface;
+
+import app.abstractions.ModuleGUI;
+
+import javax.swing.*;
+
+public class UpgradeESIAFrame extends ModuleGUI {
+    @Override
+    public void preperaGUI() throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+
+    }
+
+    @Override
+    public void initListeners() {
+
+    }
+
+    @Override
+    public void initActions() {
+
+    }
+}
