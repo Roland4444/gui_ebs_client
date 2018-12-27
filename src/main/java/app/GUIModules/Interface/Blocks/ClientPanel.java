de62944268d2035571acd07a64579aa7a295eac4
Roland4444 <turbopollux@rambler.ru>
2018-12-27 15:38:18 +0400

    class prepared

--- src/main/java/app/GUIModules/Interface/Blocks/ClientPanel.java
+++ src/main/java/app/GUIModules/Interface/Blocks/ClientPanel.java
@@ -1,18 +1,113 @@
 package app.GUIModules.Interface.Blocks;
+import Message.abstractions.Gender;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+
+import static javax.swing.JOptionPane.showMessageDialog;
 
 public class ClientPanel extends JPanel{
+    Gender[] genders = new Gender[]{Gender.F, Gender.M};
+    String[] regions = new String[]{
+            "01	Республика Адыгея",
+            "02	Республика Башкортостан",
+            "03	Республика Бурятия",
+            "04	Республика Алтай",
+            "05	Республика Дагестан",
+            "06	Республика Ингушетия",
+            "07	Кабардино-Балкарская Республика",
+            "08	Республика Калмыкия",
+            "09	Карачаево-Черкесская Республика",
+            "10	Республика Карелия",
+            "11	Республика Коми",
+            "12	Республика Марий Эл",
+            "13	Республика Мордовия",
+            "14	Республика Саха (Якутия)",
+            "15	Республика Северная Осетия - Алания",
+            "16	Республика Татарстан (Татарстан)",
+            "17	Республика Тыва",
+            "18	Удмуртская Республика",
+            "19	Республика Хакасия",
+            "20	Чеченская Республика",
+            "21	Чувашская Республика - Чувашия",
+            "22	Алтайский край",
+            "23	Краснодарский край",
+            "24	Красноярский край",
+            "25	Приморский край",
+            "26	Ставропольский край",
+            "27	Хабаровский край",
+            "28	Амурская область",
+            "29	Архангельская область",
+            "30	Астраханская область",
+            "31	Белгородская область",
+            "32	Брянская область",
+            "33	Владимирская область",
+            "34	Волгоградская область",
+            "35	Вологодская область",
+            "36	Воронежская область",
+            "37	Ивановская область",
+            "38	Иркутская область",
+            "39	Калининградская область",
+            "40	Калужская область",
+            "41	Камчатский край",
+            "42	Кемеровская область",
+            "43	Кировская область",
+            "44	Костромская область",
+            "45	Курганская область",
+            "46	Курская область",
+            "47	Ленинградская область",
+            "48	Липецкая область",
+            "49	Магаданская область",
+            "50	Московская область",
+            "51	Мурманская область",
+            "52	Нижегородская область",
+            "53	Новгородская область",
+            "54	Новосибирская область",
+            "55	Омская область",
+            "56	Оренбургская область",
+            "57	Орловская область",
+            "58	Пензенская область",
+            "59	Пермский край",
+            "60	Псковская область",
+            "61	Ростовская область",
+            "62	Рязанская область",
+            "63	Самарская область",
+            "64	Саратовская область",
+            "65	Сахалинская область",
+            "66	Свердловская область",
+            "67	Смоленская область",
+            "68	Тамбовская область",
+            "69	Тверская область",
+            "70	Томская область",
+            "71	Тульская область",
+            "72	Тюменская область",
+            "73	Ульяновская область",
+            "74	Челябинская область",
+            "75	Забайкальский край",
+            "76	Ярославская область",
+            "77	г. Москва",
+            "78	Санкт-Петербург",
+            "79	Еврейская автономная область",
+            "83	Ненецкий автономный округ",
+            "86	Ханты-Мансийский автономный округ - Югра",
+            "87	Чукотский автономный округ",
+            "89	Ямало-Ненецкий автономный округ",
+            "91	Республика Крым",
+            "92	Севастополь",
+            "99	Иные территории, включая город и космодром Байконур"
+    };
+
     public JTextField TSNILS;
     public JTextField TFIO;
-    public JList LiGender;
+    public JComboBox LiGender;
     public JTextField TBirthdate;
     public JTextField TPass;
     public JTextField TIssuedDatePass;
     public JTextField TIssuedPassID;
     public JTextField TIssuedBy;
     public JTextField TMobile;
-    public JList LiRegion;
+    public JComboBox LiRegion;
     public JTextField TFIAS;
     public JTextField TAddressStr;
     public JTextField TZIP;
@@ -63,6 +158,9 @@ public class ClientPanel extends JPanel{
     public JPanel PStreet;
     public JPanel PBirthPlace;
 
+    public  AbstractAction simple;
+
+
     public ClientPanel(){
         LSNILS = new JLabel("СНИЛС Клиента");
         LFIO= new JLabel("ФИО Клиента");
@@ -86,14 +184,14 @@ public class ClientPanel extends JPanel{
 
         TSNILS=new JTextField("",3);;
         TFIO=new JTextField("",3);;
-        LiGender=new JList();;
+        LiGender=new JComboBox<Gender>(genders);;
         TBirthdate=new JTextField("",3);;
         TPass=new JTextField("",3);
         TIssuedDatePass=new JTextField("",3);
         TIssuedPassID=new JTextField("",3);
         TIssuedBy=new JTextField("",3);
         TMobile=new JTextField("",3);
-        LiRegion=new JList();
+        LiRegion=new JComboBox<String>(regions);
         TFIAS=new JTextField("",3);
         TAddressStr=new JTextField("",3);
         TZIP=new JTextField("",3);
@@ -127,10 +225,28 @@ public class ClientPanel extends JPanel{
         this.setLayout(new GridLayout(20, 1));
 
         prepareGUI();
-
+        initListeners();
 
 
     }
+
+    public void initActions(){
+        simple = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                showMessageDialog(null, "changed"+ LiRegion.getSelectedItem());
+
+            }
+        };
+
+
+        LiRegion.addActionListener(simple);
+    }
+
+    public void initListeners (){
+        initActions();
+    }
+
 
     public void prepareGUI() {
         PFIO.add(LFIO);
